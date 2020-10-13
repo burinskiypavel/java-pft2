b06cc21973b56087e69e61290d82e1350a607da1
burinskiypavel <54236839+burinskiypavel@users.noreply.github.com>
2020-10-13 10:20:33 +0300

    add allure

--- sandbox/src/test/java/sandbox/PrimeTests.java
+++ sandbox/src/test/java/sandbox/PrimeTests.java
@@ -7,7 +7,7 @@ public class PrimeTests {
 
     @Test
     public void testPrime(){
-        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE) );
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE) );//
     }
 
     @Test(enabled = false)
